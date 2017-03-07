00bdcbafc8ac755012926639073ed41b6fcbbabb
m-ezekiel <git@m-ezekiel.com>
2017-03-07 16:58:39 -0600

    Add files, convert booleans to transactions

---
+++ GPG_association_analytics.R
@@ -0,0 +1,30 @@
+# GamePad Gaussians - Association Analytics
+# Created: 2017-03-07
+
+# Initialize libraries
+setwd("~/Documents/R_sketches/")
+library(arules)
+library(arulesViz)
+
+# Import data
+gpg_data <- read.table("2017-3-4-16-52-8_gamepadKeys.txt", sep = "\t", header = TRUE)
+attach(gpg_data)
+names(gpg_data)
+
+# Convert keypresses to transactions
+buttons <- data.frame(blue = as.logical(A1), green = as.logical(A2), red = as.logical(A3), opacity = as.logical(A4), disp = as.logical(L1), size = as.logical(R1), random = as.logical(L2), mute = as.logical(R2), select1 = as.logical(S1), select2 = as.logical(S2), up = as.logical(up), down = as.logical(down), left = as.logical(left), right = as.logical(right))
+trans <- as(buttons, "transactions")
+summary(trans)
+
+# Color value frequencies
+hist(red, col = "red")
+hist(green, col = "green")
+hist(blue, col = "blue")
+
+#support
+itemFrequency(trans)
+
+# frequent 2-itemsets
+itemsets2 <- apriori(trans, parameter = list(minlen = 2, maxlen = 2, support = 0.002, target="frequent itemsets"))
+summary(itemsets2)
+inspect(head(sort(itemsets2, by = "support"), 10))
