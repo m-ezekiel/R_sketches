8b65afd672a2b9b2f32bee984416eb7b25d3ac4e
m-ezekiel <git@m-ezekiel.com>
2017-03-07 17:29:57 -0600

    Create logical for leftJoy and rightJoy, update frequent n-itemsets

--- GPG_association_analytics.R
+++ GPG_association_analytics.R
@@ -8,11 +8,13 @@ library(arulesViz)
 
 # Import data
 gpg_data <- read.table("2017-3-4-16-52-8_gamepadKeys.txt", sep = "\t", header = TRUE)
+leftJoy <- abs(joy1_int) > 10
+rightJoy <- abs(joy2_int) > 10
 attach(gpg_data)
 names(gpg_data)
 
 # Convert keypresses to transactions
-buttons <- data.frame(blue = as.logical(A1), green = as.logical(A2), red = as.logical(A3), opacity = as.logical(A4), disp = as.logical(L1), size = as.logical(R1), random = as.logical(L2), mute = as.logical(R2), select1 = as.logical(S1), select2 = as.logical(S2), up = as.logical(up), down = as.logical(down), left = as.logical(left), right = as.logical(right))
+buttons <- data.frame(blue = as.logical(A1), green = as.logical(A2), red = as.logical(A3), opacity = as.logical(A4), disp = as.logical(L1), size = as.logical(R1), random = as.logical(L2), mute = as.logical(R2), select1 = as.logical(S1), select2 = as.logical(S2), up = as.logical(up), down = as.logical(down), left = as.logical(left), right = as.logical(right), leftJoy = as.logical(leftJoy), rightJoy = as.logical(rightJoy))
 trans <- as(buttons, "transactions")
 summary(trans)
 
@@ -24,7 +26,7 @@ hist(blue, col = "blue")
 #support
 itemFrequency(trans)
 
-# frequent 2-itemsets
-itemsets2 <- apriori(trans, parameter = list(minlen = 2, maxlen = 2, support = 0.002, target="frequent itemsets"))
+# frequent n-itemsets
+itemsets2 <- apriori(trans, parameter = list(minlen = 1, maxlen = 5, support = 0.002, target="frequent itemsets"))
 summary(itemsets2)
-inspect(head(sort(itemsets2, by = "support"), 10))
+inspect(head(sort(itemsets2, by = "support"), 20))
