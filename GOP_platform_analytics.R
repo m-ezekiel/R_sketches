f268c8bb51b38777b2cb0d62c29febb5261619e0
m-ezekiel <git@m-ezekiel.com>
2017-03-08 05:56:40 -0600

    Update paths

--- GOP_platform_analytics.R
+++ GOP_platform_analytics.R
@@ -1,7 +1,7 @@
 # 2016 Republican National Committee - Party Platform
 # Created: 2017-03-07
 
-source("wordVector_fxn.R")
-GOP_platform <- wordVector_fxn("2016_GOP_platform.txt", split = " ", removePunct = TRUE)
+source("Functions/wordVector_fxn.R")
+GOP_platform <- wordVector_fxn("Data/2016_GOP_platform.txt", split = " ", removePunct = TRUE)
 sort(table(GOP_platform))
 unique(GOP_platform)
