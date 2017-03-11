e285258ab20c00b62f74d572c4133bb8cf973e7e
m-ezekiel <git@m-ezekiel.com>
2017-03-10 23:45:33 -0600

    Remove early stage nlp sketches

--- DNC_platform_analytics.R
+++
@@ -1,7 +0,0 @@
-# 2016 Democratic National Committee - Party Platform
-# Created: 2017-03-07
-
-source("Functions/wordVector_fxn.R")
-DNC_platform <- wordVector_fxn("Data/2016_DNC_platform.txt", split = " ", removePunct = TRUE)
-sort(table(DNC_platform))
-unique(DNC_platform)
--- GOP_platform_analytics.R
+++
@@ -1,7 +0,0 @@
-# 2016 Republican National Committee - Party Platform
-# Created: 2017-03-07
-
-source("Functions/wordVector_fxn.R")
-GOP_platform <- wordVector_fxn("Data/2016_GOP_platform.txt", split = " ", removePunct = TRUE)
-sort(table(GOP_platform))
-unique(GOP_platform)
