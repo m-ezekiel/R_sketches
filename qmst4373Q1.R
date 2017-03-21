cff6bcc10a2c83e5392840f1fd90e143eff4d9c5
m-ezekiel <git@m-ezekiel.com>
2017-03-21 14:38:29 -0500

    Add file

---
+++ qmst4373Q1.R
@@ -0,0 +1,28 @@
+
+
+# 1. Text Mining
+# Retrieve data; remove stopwords, punct, convert to TDM; plot frequencies; apply algo;
+
+
+# 2. Linear Regression
+## Interpret the meaning of all three beta coefficients.
+## Discuss the statistical significance of the beta coefficients.
+## What is the value and meaning of the coefficient of determination (R-squared)?
+## Predict the value of a house price with 2 bathrooms and bricks.
+
+
+# 3.1 Association
+## Write the number of records and variables.
+## Unique countries, users and artists
+## Select records 200 and 201 and get info.
+## Make an incidence matrix.
+## Use itemFrequency() to list the support.
+## Present a horizontal barplot
+## Report the quartiles of item relative frequency.
+## Find some rules.
+## Inspect the rules.
+## Explain their meaning.
+
+
+# 3.2 Classification
+## Analyze a decision tree and confusion matrix for CarSeats data.
