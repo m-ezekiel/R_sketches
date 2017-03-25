88ad7bc2a2b71406ad04458cfa15871c64f3628f
m-ezekiel <git@m-ezekiel.com>
2017-03-25 17:14:34 -0500

    Create lastfm incidence matrix

--- qmst4373Q1.R
+++ qmst4373Q1.R
@@ -3,6 +3,9 @@
 library(tm)
 library(SnowballC)
 library(wordcloud)
+library(igraph)
+library(topicmodels)
+library(purrr)
 source("Functions/wordVector_fxn.R")
 
 ## Retrieve data, remove stopwords and punctuation
@@ -39,25 +42,53 @@ barplot(top30, horiz = TRUE, las = 1, col = "lightblue",
         xlab = "Term Frequencies",
         main = "Top 30 Words from 2016 Democratic \nand Republican Platforms")
 
-## Construct a wordcloud
+## Construct a wordcloud (requires pckg "wordcloud")
 set.seed(1)
 wordcloud(names(sortedWordFreqs), sortedWordFreqs, min.freq=70, colors=brewer.pal(6,"Dark2"))
 
-## Construct a network graph
+## Construct a network graph (requires pckg "igraph")
+# termMatrix <- sortedWordFreqs%*%t(sortedWordFreqs)
+# Network graph code keeps crashing R, no clue why. I will come back to this.
+
+## Apply cluster analysis, LDA, and/or sentiment analysis
 
 
-# 2. Linear Regression
+
+
+### 2. Linear Regression
+
+housePrices <- read.csv("Data/HousePrices.csv")
+
 ## Interpret the meaning of all three beta coefficients.
 ## Discuss the statistical significance of the beta coefficients.
 ## What is the value and meaning of the coefficient of determination (R-squared)?
 ## Predict the value of a house price with 2 bathrooms and bricks.
 
 
-# 3.1 Association
-## Write the number of records and variables.
-## Unique countries, users and artists
+
+### 3.1 Association
+library(arules)
+
+lastfm <- read.csv("Data/lastfm.csv")
+str(lastfm)
+head(lastfm)
+
+## Write the number of records and variables. (289955 records, 4 variables)
+dim(lastfm)
+
+## Unique countries, users and artists (159 coutries, 15000 users, 1004 artists)
+length(unique(lastfm$country))
+length(unique(lastfm$user))
+length(unique(lastfm$artist))
+
 ## Select records 200 and 201 and get info.
-## Make an incidence matrix.
+lastfm[200:201, ]
+
+## Make an incidence matrix: user X artist
+lastfm$user <- as.factor(lastfm$user)
+incMatrix <- table(lastfm$user, lastfm$artist)
+dim(incMatrix)
+
 ## Use itemFrequency() to list the support.
 ## Present a horizontal barplot
 ## Report the quartiles of item relative frequency.
